45ca647cb603d2a48d15a626da7c4c53c8401869
ClarkeEl360 <ClarkeEl360@northwested.org>
2024-10-01 09:40:40 -0400

    Create if_everyones_super.py

---
+++ if_everyones_super.py
@@ -0,0 +1,26 @@
+
+
+
+super = list("batman, superman, flash, metro man, deadpool")
+
+
+
+print("Who's your favorite super hero?")
+favs = input()
+
+if favs == super:
+    print("I see you are a man of class, good choice.")
+else:
+    print("Unfortunetly I don't have every super listed down, but your opinon probobly is mid, at best.")
+
+
+
+print("What was your score on your last quiz")
+quizs = float(input())
+if quizs >= 85:
+    print("Great job, you got a B, or above.")
+else:
+    print("Try better.")
+
+
+
